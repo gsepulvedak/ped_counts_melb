de3e48d00db201a2da33fae97c8e069caa7fd173
Gonzalo Sepulveda <gsepulvedak@gmail.com>
2020-08-28 23:03:50 +1000

    map ok

--- data_wrangling.R
+++ data_wrangling.R
@@ -1,5 +1,5 @@
 library(plyr)
-library(dplyr)
+library(tidyverse)
 
 # Laad datasets
 sensors <- read_csv("data/Pedestrian_Counting_System_-_Sensor_Locations (Exercise 2).csv")
@@ -32,7 +32,18 @@ updt_names <- c("RMIT Building 80", "Melbourne Central-Elizabeth St (East)")
 sensors$sensor_name <- mapvalues(sensors$sensor_name, from = curr_names, to = updt_names)
 
 # Merge fixed dataframes
-data <- left_join(peds, sensors, by = c("Sensor_Name" = "sensor_name"))
+#data <- left_join(peds, sensors, by = c("Sensor_Name" = "sensor_name"))
+
+# filter sensors with pedestrian counts data
+sensors <- sensors %>% filter(sensor_name %in% unique(peds$Sensor_Name))
+
+# Get pedestrian mean by sensor and add the column to sensors
+peds_avg <- peds %>% group_by(Sensor_Name) %>% summarise(ped_avg = mean(Hourly_Counts))
+sensors <- inner_join(sensors, peds_avg, by = c("sensor_name" = "Sensor_Name"))
+
+# Add cut levels for map legend
+sensors <- sensors %>% mutate(size_class = cut(.$ped_avg, breaks = 6, labels = FALSE),
+                              size_label = cut(.$ped_avg, breaks = 6))
 
 # Remove no longer needed objects
-rm(list = setdiff(ls(), "data"))
+rm(list = setdiff(ls(), c("sensors", "peds")))
---
+++ shiny_app.R
@@ -0,0 +1,55 @@
+library(shiny)
+library(leaflet)
+
+# Get working data set from source script
+source("data_wrangling.R")
+
+# user interface
+# ui <- 
+
+lng_center <- 144.961360
+lat_center <- -37.810316
+zoom_init <- 14.45
+fill_color <- "black"
+scale_factor = 15
+
+############################################ LEGEND #####################################
+# Create custom legend labels functions
+# Based on https://stackoverflow.com/questions/52812238/custom-legend-with-r-leaflet-circles-and-squares-in-same-plot-legends
+make_labels <- function(sizes, labels) {
+  paste0("<div style='display: inline-block;height: ", 
+         # sizes, "px;margin-top: 4px;line-height: ", 
+         sizes, "px;'>",
+         labels, "</div>")
+}
+
+make_shapes <- function(color, sizes, borders) {
+  # shapes <- gsub("circle", "50%", shapes)
+  # shapes <- gsub("square", "0%", shapes)
+  paste0(color, "; width:", sizes, "px; height:", sizes, 
+         "px; border:3px solid ", borders, 
+         "; border-radius:50%")
+}
+
+sizes <- unique(sensors$size_class)*3
+label_levels <- levels(sensors$size_label)
+
+labels <- make_labels(sizes, label_levels)
+colors_shapes <- make_shapes(fill_color, sizes, fill_color)
+
+##########################################################################################
+
+
+### Map
+leaflet(data = sensors) %>% 
+  setView(lng = lng_center, lat = lat_center, zoom = zoom_init) %>%
+  addProviderTiles("CartoDB.Voyager") %>%
+  addCircles(lng = ~longitude, lat = ~latitude, radius = ~ped_avg/scale_factor,
+             stroke = FALSE, fillColor = fill_color, fillOpacity = 0.5, 
+             label = ~paste("Sensor:", sensor_name),
+             highlightOptions = highlightOptions(fillColor = "green", bringToFront = FALSE))# %>% 
+  # addLegend("bottomright", colors = colors_shapes, labels = labels)
+
+
+  
+### Line small multiples
